a87ac1d64c2fdcf17ab2de60a7ea781509dfad72
paulomelila <26441584+paulomelila@users.noreply.github.com>
2025-09-02 11:37:19 -0300

    added 003. Dynamic Typing

---
+++ Exercises/003. Dynamic Typing.py
@@ -0,0 +1,13 @@
+# Example of dynamic typing in Python
+
+# 'x' variable receives an integer value
+x = 5
+
+# print the current type of the variable
+print("Variable type:", type(x))
+
+# 'x' variable receives a string value
+x = "Hello"
+
+# print the current type of the variable
+print("Variable type:", type(x))
