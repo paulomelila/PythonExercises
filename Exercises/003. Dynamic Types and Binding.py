a511aac3637688fa91f0eaa3e58493db756ead2a
paulomelila <26441584+paulomelila@users.noreply.github.com>
2025-09-02 15:23:05 -0300

    updated 003. Dynamic Types and Binding

---
+++ Exercises/003. Dynamic Types and Binding.py
@@ -0,0 +1,13 @@
+# Example of dynamic types and binding in Python
+
+# binding of an integer value to a variable x
+x = 5
+
+# prints the current type of the variable
+print("Variable type:", type(x))
+
+# binding of a String value to the same variable x
+x = "Hello"
+
+# prints the current type of the variable x
+print("Variable type:", type(x))
