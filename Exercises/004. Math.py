b2c570fc17b80def81e7ce43bde0feac5a501b2c
paulomelila <26441584+paulomelila@users.noreply.github.com>
2025-09-02 11:48:49 -0300

    added 004. Math

---
+++ Exercises/004. Math.py
@@ -0,0 +1,13 @@
+# Example of use of a standard Python library
+
+import math # imports the math module that provides access to mathematical functions
+
+# Calculating the square root of a number
+
+number = 81
+
+# FUNCTION: sqrt(number) -> calculates the square root of a given number
+square_root = math.sqrt(number)
+
+# prints the square root of the number
+print("The square root of", number, "is", square_root)
