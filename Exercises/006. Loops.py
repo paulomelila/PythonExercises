a47208939ca4fbcd723222f6ee2191dad64d90e1
paulomelila <26441584+paulomelila@users.noreply.github.com>
2025-09-02 15:12:45 -0300

    updated 006. Loops added 008. Variables

--- Exercises/006. Loops.py
+++ Exercises/006. Loops.py
@@ -6,10 +6,13 @@ to print 1 -> 10, we have to add +1
 """
 
 print("0 -> 9:")
+
 for i in range (10):
 
     # prints 0->9
     print(i)
+
+print("\n")
 
 print("1 -> 10:")
 for i in range(10):
@@ -17,8 +20,21 @@ for i in range(10):
     # prints 1->10
     print(i + 1)
 
+print("\n")
+
 print("1 -> 10: (same line)")
 for i in range(10):
 
     # prints 1->10 in the same line
     print(i+1, end=" ")
+
+print("\n")
+
+"""
+Loop that increases the counter.
+"""
+
+counter = 0 # sets counter to zero
+while counter < 5: # while counter is at 0->4 the loop will run
+    print("Counter is", counter)
+    counter += 1 # increases the counter value for the next loop
---
+++ Exercises/008. Variables.py
@@ -0,0 +1,26 @@
+"""
+There are many variables in Python, the most used ones are:
+
+Integers (int): Whole numbers, positive or negative, without a decimal point
+Floats (float): Numbers with a decimal point, representing real numbers
+Strings (str): Sequences of characters, enclosed in single or double quotes.
+Booleans (bool): Represent truth values, either True or False.
+
+"""
+
+name = "Albert"  # String (str)
+age = 33    # Integer (int)
+height = 1.76 # Float (float)
+is_of_legal_age = age >= 18 # Boolean (bool)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_of_legal_age))
+
+print(name, f"is {age} years-old,", f"{height}m tall")
+
+if is_of_legal_age:
+    print(f"{name} is of legal age")
+else:
+    print(f"{name} is a minor")
