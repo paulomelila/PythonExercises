71a82b954df453d67f0802aa054061439154bd5c
paulomelila <26441584+paulomelila@users.noreply.github.com>
2025-09-02 11:29:39 -0300

    added hello world and basic function

---
+++ Exercises/001. Hello, world.py
@@ -0,0 +1,2 @@
+#   this is my first code. 'print' writes the message inside ' ' in the console
+print('Hello, world!')
---
+++ Exercises/002. Functions.py
@@ -0,0 +1,9 @@
+# defining a function that returns the result of the sum between two given numbers
+def sum_of_two_numbers(a, b):
+    return a + b
+
+# creating a variable that stores the result of the function
+result = sum_of_two_numbers(56, 25)
+
+# printing the result
+print("The result of the sum is:", result)
