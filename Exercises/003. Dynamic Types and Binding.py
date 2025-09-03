26d3511e44da915a05454e1511ceb9b14cd7bd0e
paulomelila <26441584+paulomelila@users.noreply.github.com>
2025-09-03 15:24:46 -0300

    updated 003

--- Exercises/003. Dynamic Types and Binding.py
+++ Exercises/003. Dynamic Types and Binding.py
@@ -11,3 +11,20 @@ x = "Hello"
 
 # prints the current type of the variable x
 print("Variable type:", type(x))
+
+# binding a new value to the same variable
+y = 10
+print(y)
+y += 2  # same as y = y + 2
+y -= 2  # same as y = y - 2
+y *= 2  # same as y = y * 2
+y /= 2  # same as y = y / 2
+y %= 2  # same as y = y % 2
+print(y)
+
+# multiple binding
+a, b, c = 1, 2, 3 # binds multiple variables in the same line
+print(a, b, c)
+# swap values between them
+a, b, c = c, a, b
+print(a, b, c)
