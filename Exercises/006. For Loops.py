1efb5c8e739314423553c1fa65c3a1e16cf7a5c4
paulomelila <26441584+paulomelila@users.noreply.github.com>
2025-09-14 12:44:07 -0300

    added While Loops

--- Exercises/006. For Loops.py
+++ Exercises/006. For Loops.py
@@ -107,3 +107,13 @@ counter = 0 # sets counter to zero
 while counter < 5: # while counter is at 0->4 the loop will run
     print("Counter is", counter)
     counter += 1 # increases the counter value for the next loop
+
+"""
+While loop that prints a message until user enters 'exit'.
+"""
+message = input("Type 'exit' to stop the loop:")
+while message != "exit":
+    print("You typed:", message)
+    message = input("Type 'exit' to stop the loop:")
+print("Loop exited.")
+print("\n")
