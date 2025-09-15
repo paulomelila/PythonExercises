d9437e662094588c6b03eee231b1ba2c19b17340
paulomelila <26441584+paulomelila@users.noreply.github.com>
2025-09-15 10:34:55 -0300

    added example of docstrings usage in Python

---
+++ Exercises/026. Docstrings.py
@@ -0,0 +1,7 @@
+# example of use of docstrings in Python
+def return_zero():
+    """This function returns the number 0""" # docstring
+    return 0
+
+# print the docstring
+print(help(return_zero))
